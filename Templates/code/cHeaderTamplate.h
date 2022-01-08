b5441238e0b7d357d5478e8ed1260aef22f12666
Jeff Hatton <jeahatton@gmail.com>
2022-01-07 22:55:00 -0500

    Snippet work

---
+++ Templates/code/cHeaderTamplate.h
@@ -0,0 +1,25 @@
+/**
+ * @file cHeaderTamplate.h
+ * @author Jeff Hatton
+ * @brief 
+ * @date 2022-01-07
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#ifndef cHeaderTamplate
+#define cHeaderTamplate
+
+// Exported Types ************************************
+
+// Exported Constants ************************************
+
+// Exported Objects **************************************
+
+// Exported Macros ***************************************
+
+// Exported Functions ************************************
+
+// END cHeaderTamplate
+#endif
---
+++ Templates/code/cSourceTemplate.c
@@ -0,0 +1,23 @@
+/**
+ * @file cSourceTemplate.c
+ * @author Jeff Hatton
+ * @brief 
+ * @date 2022-01-07
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+// Private Types ****************************************
+
+// Constants ********************************************
+
+// Macros ***********************************************
+
+// Functions Prototypes *********************************
+
+// Private Variables ************************************
+
+// Private Functions *************************************
+
+// Public Functions *************************************
